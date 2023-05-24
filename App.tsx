727c5a5d0c481d3eca974d8eec8bd2e7a437f1f0
Tyriix <konrad.barszcz1@gmail.com>
2023-05-24 22:10:25 +0200

    Added splash screen for launch

--- App.tsx
+++ App.tsx
@@ -7,20 +7,36 @@ import {
 
 import { AppNavigator } from './navigation/AppNavigator';
 import { theme } from './themes/appTheme';
+import { useEffect, useRef } from 'react';
+import * as SplashScreen from 'expo-splash-screen'
 
 export default function App() {
+
   let [fontsLoaded] = useFonts({
     Audiowide_400Regular,
     Aldrich_400Regular,
   });
 
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
   if (!fontsLoaded) {
-    return;
+    return undefined;
   } else {
+    setTimeout(() => {
+      SplashScreen.hideAsync();
+    }, 2000);
+    console.log("???")
+  }
+
     return (
       <NativeBaseProvider theme={theme}>
         <AppNavigator />
       </NativeBaseProvider>
     );
-  }
+  
 }
