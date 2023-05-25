7a26ffa6390e0837c07e6d7da4b72da75b994f71
Tyriix <konrad.barszcz1@gmail.com>
2023-05-25 20:50:35 +0200

    Added background music with looping

--- App.tsx
+++ App.tsx
@@ -1,26 +1,64 @@
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider } from "native-base";
 import {
   useFonts,
   Audiowide_400Regular,
   Aldrich_400Regular,
-} from '@expo-google-fonts/dev';
+} from "@expo-google-fonts/dev";
 
-import { AppNavigator } from './navigation/AppNavigator';
-import { theme } from './themes/appTheme';
-import { useEffect, useRef } from 'react';
-import * as SplashScreen from 'expo-splash-screen'
+import { AppNavigator } from "./navigation/AppNavigator";
+import { theme } from "./themes/appTheme";
+import { useEffect, useRef, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { Audio } from "expo-av";
+import React from "react";
 
 export default function App() {
+  const [sound, setSound] = useState<Audio.Sound | null>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+  const [finish, setFinish] = useState<boolean>(true);
 
+  const playSound = async () => {
+    const source = require("./assets/background-music.mp3");
+    const { sound } = await Audio.Sound.createAsync(source,
+      {
+        shouldPlay: true,
+        isLooping: true
+      }
+    );
+    setSound(sound);
+    if (sound != null) {
+      await sound.playAsync();
+      setIsPlaying(true)
+
+    }
+      else console.log("Sound not found.");
+  };
+
+  const stopSound = async () => {
+    if (sound != null) await sound.stopAsync();
+    else console.log("Sound not found");
+  };
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  ///////////
   let [fontsLoaded] = useFonts({
     Audiowide_400Regular,
     Aldrich_400Regular,
   });
 
   useEffect(() => {
+    
     async function prepare() {
       await SplashScreen.preventAutoHideAsync();
     }
+    playSound();
     prepare();
   }, []);
 
@@ -32,10 +70,9 @@ export default function App() {
     }, 2000);
   }
 
-    return (
-      <NativeBaseProvider theme={theme}>
-        <AppNavigator />
-      </NativeBaseProvider>
-    );
-  
+  return (
+    <NativeBaseProvider theme={theme}>
+      <AppNavigator />
+    </NativeBaseProvider>
+  );
 }
